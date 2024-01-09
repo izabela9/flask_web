a83cda763d5e5f10241535bbf01ce216e20c39da
izabela9 <isabelanelaj1@gmail.com>
2024-01-09 15:26:38 +0100

    update code

---
+++ website/static/index.js
@@ -0,0 +1,10 @@
+
+function deleteNote(noteId) {
+    fetch('/delete-note', {
+        method: "POST",
+        body: JSON.stringify({ noteId }),
+    })
+        .then((_res) => {
+            window.location.href = "/";
+        });
+}
